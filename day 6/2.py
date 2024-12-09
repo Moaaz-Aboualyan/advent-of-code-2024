145133a1b91d6e74b882066ab37cd34c97e6aa2e
bensnake <moaazah2@gmail.com>
2024-12-09 11:44:57 +0800

    day 6 part 2

--- day 6/2.py
+++ day 6/2.py
@@ -1,52 +1,38 @@
 input = open("./input.txt", "r")
 lines = input.readlines()
-direction = 0
+x, y, direction = 0, 0 , 0
+chars = ["^", ">", "v", "<"]
 directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-chars = ["^", ">",  "v", "<"]
-x = 0
-y = 0
 for i in range(len(lines)):
     lines[i] = lines[i].strip()
-    for j in range(len(chars)):
-        if lines[i].find(chars[j][0]) != -1:
-            x = lines[i].find(chars[j][0])
+    for j in range(len(lines[i])):
+        if lines[i][j] in chars:
+            x = j
             y = i
-            direction = j
-            break
+            direction = chars.index(lines[i][j])
 
-def isLoop(lines, x, y, direction):
+def path(lines, x, y, direction):
+    visited = set()
     while x >= 0 and x < len(lines[0]) and y >= 0 and y < len(lines):
-        if lines[y][x] == "#":
+        if (x, y, direction) in visited:
+            return 1
+        if (lines[y][x] == "#"):
             x -= directions[direction][0]
             y -= directions[direction][1]
             direction += 1
-            if direction > len(directions) - 1:
+            if direction > 3:
                 direction = 0
-            x += directions[direction][0]
-            y += directions[direction][1]
-
-        lines[y] = lines[y][:x] + chars[direction] + lines[y][x + 1:]
+        visited.add((x, y, direction))
+        lines[y] = lines[y][:x] + chars[direction] + lines[y][x+1:]
         x += directions[direction][0]
         y += directions[direction][1]
-        try:
-            if lines[y][x] == chars[direction]:
-                return 1
-        except:
-            break
-
     return 0
-    
+
 total = 0
-iterations = 0
 for i in range(len(lines)):
     for j in range(len(lines[i])):
-        tempLines = lines.copy()
-        iterations += 1
-#        print(iterations, total)
-        tempLines[i] = tempLines[i][:j] + "#" + tempLines[i][j + 1:]
-        #total += isLoop(tempLines, x, y, direction)
-        if isLoop(tempLines, x, y, direction):
-            print(tempLines)
-
-
+        if lines[i][j] == ".":
+            tempLines = lines.copy()
+            tempLines[i] = tempLines[i][:j] + "#" + tempLines[i][j+1:]
+            total += path(tempLines, x, y, direction)
 print(total)
